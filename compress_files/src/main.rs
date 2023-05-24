6505ec877f431317d866f15c5446fbd809839112
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-05-24 09:22:30 +0530

    Add all

--- compress_files/src/main.rs
+++ compress_files/src/main.rs
@@ -4,21 +4,56 @@ extern crate flate2;
 use flate2::Compression;
 use flate2::write::GzEncoder;
 
+
+
 // inorder to except arguments from user in cli
 use std::env::args;
 
-// 
+
+
+// when you convert a file to a compressed version 
+// (essentially we are copying contents of that file)
 use std::fs::File;
-use std::io::copy;
+// first we read that file
 use std::io::BufReader;
+// then we copy the contents of that file
+use std::io::copy;
+// in order to output "how much time the process took" (BENCHMARKING lol!)
 use std::time::Instant;
 
 
 fn main() {
+
+    // if the usage is not correct we'll tell them how to use it
     if args().len() != 3 {
         eprintln!("Usage: `source` `target`");
+        eprintln!("source :-------->  file to compress");
+        eprintln!(" target:-------->  how should it be named after compression");
         return;
     }
 
+    // 2nd word is the file. `input` is the file that we will be reading
+    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+
+    // creating an output file
+    let output = File::create(args().nth(2).unwrap()).unwrap();
+
+    // turn on the inlay hint (ctrl+alt here) 
+    // see that GzEncoder::new(w , level ) you are writing to ouput
+    let mut encoder = GzEncoder::new(output, Compression::default());
+
+ 
+    
+    
+    copy(&mut input, &mut encoder).unwrap();
+    let output = encoder.finish().unwrap();
+
+    // info on compression 
+    println!(
+        "Source len: {:?}",
+        input.get_ref().metadata().unwrap().len()
+    );
+    println!("Target len: {:?}", output.metadata().unwrap().len());
+    
 }
 
