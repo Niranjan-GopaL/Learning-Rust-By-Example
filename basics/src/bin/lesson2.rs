7c2f42877704376dabd31891736e1bae8d5e3618
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-05-25 07:27:33 +0530

    Good documentation of important concepts of Mathcing patterns

--- basics/src/bin/lesson2.rs
+++ basics/src/bin/lesson2.rs
@@ -3,6 +3,7 @@ fn lesson_2() -> i32 {
     // most important thing about MATCH :-  THEY MUST BE EXHAUSTIVE
     // all possibilities MUST BE ACCOUNTED FOR in matching
     let some_bool = true;
+    let some_no = 123;
 
     // THESE ARE FAT ARROWS. LOLLLLLLLLLLLLLLLL!
     //  matches evaluates EXPRESSION AND NOT STATEMENTS (hence the comma after each match)
@@ -23,6 +24,47 @@ fn lesson_2() -> i32 {
         // false => println!("{some_bool}")  <--- second expression 
         false => println!("{some_bool}"),
     }
+
+
+    //  how to account for all possibilites
+    match some_no {
+        1 => println!("{some_no}"),
+        2 => println!("{some_no}"),
+        3 => println!("{some_no}"),
+        4 => println!("{some_no}"),
+        // anything else will give this
+        _ => println!("Big")
+    }
+
+
+    // If you added a new possibility while coding compiler will intstruct that
+    // you missed a case . So its helpful in writing clean code . 
+    // Compiler wont check if else
+
+
+    /*  IMPORTANT THING TO NOTICE
+
+        match some_no {
+        1 => println!("{some_no}"),
+        2 => println!("{some_no}"),
+        3 => println!("{some_no}"),
+        4 => println!("{some_no}"),
+        _ => println!("Big")
+
+        THE SET OF KEYS MUST BE EXHAUSTIVE AND EACH MUST BE UNIQUE !!!!!
+        THAT MEANS YOU CAN'T DO SOMETHING LIKE :-
+
+        match some_no {
+        some_no > 100 => println!("{some_no}"),
+        some_no = 100 => println!("{some_no}"),
+        some_no < 100 => println!("{some_no}"),
+        }
+
+        eventhough we exhaust all possibilites of some_no , 
+        the left hand side is {F,T,F} and therefore not unique
+
+     */
+
 
     return 0;
 }
