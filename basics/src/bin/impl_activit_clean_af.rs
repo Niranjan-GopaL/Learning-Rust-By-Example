f1d2bd585b827f669d95ae51dd9c89214c70a082
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-05-28 00:32:42 +0530

    impl activity COMPLETED CLEAN CODE

--- basics/src/bin/impl_activit_clean_af.rs
+++ basics/src/bin/impl_activit_clean_af.rs
@@ -1,3 +1,4 @@
+// <-------------------------------------- COLOR ENUM IS CREATED HERE --------------------------------
 enum Color {
     _Green,
     _Blue,
@@ -6,22 +7,86 @@ enum Color {
 }
 
 impl Color{
-    fn print() {
-
+    fn print(&self) {
+        match self  {
+            Color::_Black => println!("Black"),
+            Color::_Red => println!("Red"),
+            Color::_Blue => println!("_Blue"),
+            Color::_Green => println!("Green")
+        }
     }
 }
+// <----------------------------------------------------------------------------------------------------
 
 
+
+// <-------------------------------------- DIMENSION STRUCT IS CREATED HERE --------------------------------
 struct Dimension{
-
+    _height: f32,
+    _length: f32,
+    _breadth: f32,
 }
 
 
+impl Dimension{
+    fn print(&self){
+        println!("_height: {}", self._height);
+        println!("_length: {}", self._length);
+        println!("_breadth: {}", self._breadth);
+    }
+}
+// <----------------------------------------------------------------------------------------------------
+
+
+
+
+// <-------------------------------------- SHIPPING BOX IS CREATED HERE --------------------------------
 struct ShippingBox{
-
+    _dimensions: Dimension,
+    _color : Color,
+    _weight  : f32,
 }
+
+impl ShippingBox{
+    fn new(_dimensions: Dimension, _color: Color, _weight: f32) -> Self {
+        Self { _dimensions, _color, _weight }
+    }
+    
+    fn print(&self){
+        self._dimensions.print();
+        self._color.print();
+        println!("{:?}",self._weight);
+    }
+}
+// <----------------------------------------------------------------------------------------------------
+
+
+
 
 
 fn main() {
+
+    // Lets create a small box
+    let small_dimensions = Dimension{
+        _height: 1.2,
+        _length: 0.3,
+        _breadth:5.4};
+    let small_color = Color::_Green;
+    let small_weight = 23.4;
+
+    let small_box = ShippingBox::new(small_dimensions, small_color, small_weight);
+    small_box.print();
     
+    
+    // Let's create a large box
+    let large_dimensions = Dimension{
+        _height: 110000.231,
+        _length: 1214312.32,
+        _breadth:4123412.123};
+    let large_color = Color::_Black;
+    let large_weight = 23.4;
+
+    let large_box = ShippingBox::new(large_dimensions, large_color, large_weight);
+    large_box.print();
+
 }
