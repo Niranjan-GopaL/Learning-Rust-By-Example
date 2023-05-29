acad3cad2bcc99d1b5e0c7c40db46724ff7596bf
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-05-29 16:46:49 +0530

    Better and simpler version of read_to_string() function

--- basics/src/bin/26_result_enum.rs
+++ basics/src/bin/26_result_enum.rs
@@ -54,12 +54,61 @@ fn main() {
         // error, the way error should be handled SHOULD BE DETERMINED BY CALLER
         
         let _user_name = _read_username_from_file();
+        
+}
+    
 
+
+
+
+
+// <--------------- awesome version -----------------------------
+
+/// Better version of read_username_from_file()
+fn _read_username_from_file_()->Result<String,io::Error>  {
+    
+        //` ? operator`
+        // used in a function that returns `Result` or `Option` 
+        // (or another type that implements `FromResidual`)
+
+        // `?` means 2 possibilities (actually its just  the replacement for the mannual match we did)
+
+        //  1 -> returns whatever the fn returns 
+        //  2 -> makes the function return with the error it gave 
+        //      (it does not panic rather lets the caller decide what to do with the error)
+
+
+        let mut f = File::open("username.txt")?;
+        let mut s = String::new();
+        f.read_to_string(&mut s)?;
+        Ok(s)
+
+        
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// IMPORTNAT THING I LEARNT WHEN WRITING FNs in RUST IS
+/// --> JUST KEEP ON WRITING, YOU'LL GET ERROR BUT ONCE YOU FINISH WRITING THE FN
+/// YOU CAN HAVE THE COMPILER HELP YOU EXACTLY IN WHAT YOU WANT TO DO
+/// 
+/// so do NOT DEBUG IN MIDDLE OF WRIITNG A FN 
 fn _read_username_from_file() -> Result<String,io::Error> {
     let _f = File::open("user_name.txt");
-
+    
+    // no idea why _f has to be mut but compiler told me so
+    // I found out why --> read_to_string() accepts a mutable reference to file THAT'S WHY  
     let mut _f = match _f {
         Ok(_f) => _f,
         Err(e) => return Err(e),
@@ -67,14 +116,21 @@ fn _read_username_from_file() -> Result<String,io::Error> {
         // expected unit type `()`
         // found enum `std::result::Result̥<_, std::io::Error>`̥
 
-        // Therfore YOU NEED TO SPECTIFY THE RETUR TYPE OF FN BEFOREHAND
+        // Therfore YOU NEED TO SPECTIFY THE RETUR TYPE OF FN 
         // at this point fn seems to return Result<_,io::Error>
+
+        // actually just like said in the fucntion docstring 
+        // you don't have to worry about errors now, COMPLETE THE FN then debug
     };
 
     let mut s = String::new();
 
+    // notice that there is no semicolon. THis is what the entire fn
+    // evaluates to
     match _f.read_to_string(&mut s) {
         Ok(_) => Ok(s),
         Err(e) => Err(e),
     }
 }
+
+
