f9f253364e624475b5b0342a842b467bdd335db3
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-05-30 16:36:31 +0530

    traits' default behavior

--- basics/src/bin/32_traits_intro.rs
+++ basics/src/bin/32_traits_intro.rs
@@ -8,6 +8,7 @@
 
 
 
+//<-------------------------------------------------------------------
 pub struct NewArticle {
     pub author: String,
     pub headline: String,
@@ -16,11 +17,20 @@ pub struct NewArticle {
 
 
 impl Summary for NewArticle {
-    fn summarize(&self) -> String {
+    fn summarize_1(&self) -> String {
         format!("{} {}", self.author, self. headline)
     }
-}
 
+    /// we are overriding the default summarize_2
+    fn summarize_2(&self) -> String {
+        String::from("THIS OVER-RODE DEFAULT")
+    }
+}
+//<-------------------------------------------------------------------
+
+
+
+//<-------------------------------------------------------------------
 pub struct Tweet {
     pub username: String,
     pub content: String,
@@ -29,10 +39,18 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
-    fn summarize(&self) -> String {
+    fn summarize_1(&self) -> String {
         format!("{} {}", self.username, self.content)
     }
+
+    // remember this - in syntax we don't 
+    // have to state the trait fn in impl block
+    // learn more about the syntax. Why pub? etc
+    // fn summarize_2(&self) -> String;
 }
+//<-------------------------------------------------------------------
+
+
 
 
 // Now we may want to implement function that can be
@@ -41,15 +59,27 @@ impl Summary for Tweet {
 /// We now have the ability to SUMMARIZE BOTH 
 /// Tweet and NewArticle (even thought they are of different TYPES)
 pub trait Summary {
-    // --> we only need to specify the fn signature
-    // --> we don't specify fn body because we don't 
-    //     want to dictate how the type "impl" the fn
-
+    /*
+    --> we only need to specify the fn signature
+    --> we don't specify fn body because we don't 
+        want to dictate how the type "impl" the fn
+    */
 
     /// "reference to self" and returns a "string"
-    fn summarize(&self) -> String ;
+    fn summarize_1(&self) -> String ;
+    
+    /*
+    sometimes we may need that all trait functions have 
+    certain DEFAULT IMPLEMENTATION for body.
+    These can be overridden IF wewant to design impl ourself
+    */
+    fn summarize_2(&self) -> String {
+        String::from("DEFAULT IMPLEMENTATION OF summarize_2 ")
+    }
     
 }
+
+
 
 
 
@@ -68,7 +98,13 @@ fn main(){
         retweet: false
     };
 
-    println!("{}",tweet.summarize());
-    println!("{}",news_article.summarize());
+    // instances share same functionality
+    println!("{}",tweet.summarize_1());
+    println!("{}",news_article.summarize_1());
+
+    // tweet default impl of trait summarize_2
+    // news_article has it's own impl of trait summarize_2 
+    println!("{}",tweet.summarize_2());
+    println!("{}",news_article.summarize_2());
 
 }
