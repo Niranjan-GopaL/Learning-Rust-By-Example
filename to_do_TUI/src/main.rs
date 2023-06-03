b20cfd10bd6fb93bec49c13fc7cdbaaf153e0dea
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-03 22:05:14 +0530

    Got to do to be displayed on screen

--- to_do_TUI/src/main.rs
+++ to_do_TUI/src/main.rs
@@ -11,11 +11,35 @@ fn main() {
 
     initscr();
 
-    addstr("Hello, world!");
+    let mut quit = false;
+    
+    let mut to_do = vec![
+        "execute the plan",
+        "I am happy",
+        "Make tea",
+        "Learn Cpp",
+        "Learn more on rust",
+        "Learn more twcss",
+        "Learn more MUI",
+    ];
 
-    refresh();
-
-    getch();
+    
+    
+    while !quit {
+        
+        for (row,todo)  in to_do.iter().enumerate() {
+            mv(row as i32, 0);
+            addstr(*&todo);
+        }
+        
+        refresh();
+        
+        let key = getch();
+        match key as u8 as char {
+            'q' => quit = true,
+            other => println!("{}",other)
+        }
+    }
     
     endwin();
 }
