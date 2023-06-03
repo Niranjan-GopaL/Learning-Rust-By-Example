418a08412c3c436e4d7b9be145d83102b31e5dec
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-03 16:17:04 +0530

    basic overview of the first part. No comments. Not refactored. Just a working skeleton

--- mini-grep/src/main.rs
+++ mini-grep/src/main.rs
@@ -1,3 +1,19 @@
+//! minigrep impl
+use std::{env,fs};
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = env::args().collect();
+    
+    let query = &args[1];
+    let filepath = &args[2];
+    
+    println!("Searching for {}",query);
+    println!("in file {}",filepath);
+
+    let contents = fs::read_to_string(filepath).expect("Something went wrong reading a file ...");
+
+    println!("{}",contents);
+
 }
