93824adbb4a01cebff8a2093722579f230ca4c74
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-03 14:44:59 +0530

    returns some type that can impl a certain trait

--- basics/src/bin/33_traits_more.rs
+++ basics/src/bin/33_traits_more.rs
@@ -4,16 +4,22 @@
 //! [4] A nice quirk 
 //! [5] A good example
 
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+
+
+
 struct Foo1{
     bar1: String,
     bar2: String,
     bar3: String,
 }
 
-impl Speak_bar for  Foo1 {
-    fn speak_bar(&self) -> String{
-        // concat!("I GOT THIS FROM THE RUSTLANG DOCS",String::from(&self.bar1)).to_owned()
-        // ["hello", "world"].join(" ")
+impl SpeakBar for  Foo1 {
+
+    fn speak_bar_trait(&self) -> String{
+        // ["hello", "world"].join(" ") 
 
         // MY FIRST FEVER FINDING ON MY OWN FROM DOC
         ["MY FIRST FEVER FINDING ON MY OWN FROM DOC".to_string(),String::from(&self.bar1)].join(" ")
@@ -26,29 +32,83 @@ struct Foo2{
     bar3: String,
 }
 
-impl Speak_bar for  Foo2 {
+impl SpeakBar for  Foo2 {
     // it can still impl the DEFAULT behavior of the trait fn 
 }
 
-trait Speak_bar{
-    fn speak_bar(&self) -> String{
+
+
+
+
+
+
+/// Traits need UpperCamelCase
+trait SpeakBar{
+    fn speak_bar_trait(&self) -> String{
      String::from("THI IS THE DEFAULT IMPL")
     }
 }
 
+
+
+// <-------------------------------------------------------------------------
+
+/// this means param is some type that has SpeakBar trait {  "that immplements SpeakBar trait" }
+fn notify_1(param:&impl SpeakBar) 
+{   
+        // THIS IS THE "SYNTAX-SUGARED" WAY OF below
+}
+
+/// THIS IS THE REA DEAL. takes in a generic type that as SpeakBar trait
+fn notify_2<T:SpeakBar>(param:T) 
+{
+    
+}
 // <-------------------------------------------------------------------------
 
 
-fn notify_1(param:&impl Speak_bar) 
-{   
-    
-}
 
 
-fn notify_2<T:Speak_bar>(param:T) 
+
+
+// <-------------------------------------------------------------------------
+
+fn func1<T:>(param1:T, param2:T) 
 {
-
+    // this means that both takes in a generic type that implements SpeakBar trait
+    // AND THAT both params are of same type
 }
+
+
+fn func2(param1:&impl SpeakBar, param2:&impl SpeakBar) 
+{
+    // this means that both takes in a type that implements SpeakBar trait
+    // BUT BOTH CAN BE OF DIFF TYPE 
+}
+ // <-------------------------------------------------------------------------
+ 
+ 
+ 
+ 
+ // <-------------------------------------------------------------------------
+
+/// Returns some type that has a specific trait
+fn make_speakbar_able() -> impl SpeakBar {
+    Foo1{
+        bar1: "bar1_of_Foo_1".to_string(),
+        bar2: "bar2_of_Foo_1".to_string(),
+        bar3: "bar3_of_Foo_1".to_string(),        
+    }
+}
+ 
+ 
+ 
+ // <-------------------------------------------------------------------------
+
+
+
+
+
 
 fn main(){
     let foo_1 = Foo1{
@@ -62,5 +122,8 @@ fn main(){
         bar2: "bar2_of_Foo2".to_string(),
         bar3: "bar3_of_Foo2".to_string(),
     };
+
+    println!("{:?}",foo_1.speak_bar_trait());
+    println!("{:?}",foo_2.speak_bar_trait());
 
 }
