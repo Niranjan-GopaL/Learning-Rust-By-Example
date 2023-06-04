f7c67fa888f7e8fac0296c9612259338f3b66649
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-04 16:58:24 +0530

    Memoization pattern implemented

--- basics/src/bin/35_closure_more.rs
+++ basics/src/bin/35_closure_more.rs
@@ -43,6 +43,112 @@ fn generate_workout(intensity:u32,random:u32)
 
 
 
+
+
+
+
+
+
+
+
+
+
+/// generate_workout() using closure
+fn generate_workout_2(intensity:u32,random:u32)
+{
+    // closure. Logic is in a single place
+    let simulated_expensive_calculations_result = 
+    |intensity|
+    {
+        println!("Calculating slowly ....");
+        thread::sleep(Duration::from_secs(3)); // sleeps for 3 seconds
+        intensity * 2 / 3 + 1
+    };
+
+    if intensity > 25 
+    {
+        // we do the same expensive function twice
+        println!("Today you need to do {:?} pushups",simulated_expensive_calculations_result(intensity));
+        println!("Today you need to do {:?} situps",simulated_expensive_calculations_result(intensity));
+    }else 
+    {
+        if random == 3 {
+            println!("Take a break today, remember to stay hydrated")
+        }else {
+            // we do the same expensive function
+            println!("Today walk for {} minutes", simulated_expensive_calculations_result(intensity))
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// generate_workout() using closure and using Memoization patterns
+fn generate_workout_3(intensity:u32,random:u32)
+{
+    let mut cached_result = // storing in the Cacher struct to use the memoization pattern
+    Cacher::new(
+    |intensity|
+    {
+        println!("Calculating slowly ....");
+        thread::sleep(Duration::from_secs(3)); // sleeps for 3 seconds
+        intensity * 2 / 3 + 1
+    });
+
+
+    if intensity > 25 
+    {
+        // we do the same expensive function twice
+        println!("Today you need to do {:?} pushups",cached_result.value(intensity));
+        println!("Today you need to do {:?} situps",cached_result.value(intensity));
+    }else 
+    {
+        if random == 3 {
+            println!("Take a break today, remember to stay hydrated")
+        }else {
+            // we do the same expensive function
+            println!("Today walk for {} minutes", cached_result.value(intensity))
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //<---------------------------------------------------------------------------------------------->
 
 /// This is called a "Memoisation pattern" 
@@ -56,6 +162,33 @@ where T: Fn(u32) -> u32
     value:Option<u32> // when we initialize the struct instance, value will be None BUT after that we'll sotre
 }                     // BUT after that we'll store the result of that closure in value field  
 
+
+impl<T> Cacher<T>
+where T: Fn(u32) -> u32
+{
+    fn new(calculation:T) -> Self {
+        Self{calculation, value: None}
+    }
+
+
+
+    fn value(&mut self,arg:u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            // initialy when struct_instance.value(1234) is called
+            // we pass 1234 to the calculation field which holds the closure
+            // then value will store the closure's reuslt
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+
+}
+
+
 //<---------------------------------------------------------------------------------------------->
 
 
@@ -67,9 +200,13 @@ where T: Fn(u32) -> u32
 
 
 fn main() {
-    let simulated_intensity = 2;
+    let simulated_intensity = 200;
     let simulated_rand_number = 2;
 
+    // compare how these perform :- (uncomment one and comment the rest)
 
+    // generate_workout(simulated_intensity, simulated_rand_number);
+    // generate_workout_2(simulated_intensity, simulated_rand_number);  
+    generate_workout_3(simulated_intensity, simulated_rand_number);
 
 }
