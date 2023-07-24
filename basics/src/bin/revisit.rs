4b9f036f91b603556cc3c9e4da5d8830ee1c97bc
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-07-24 23:29:07 +0530

    Revisting all concepts and tricks

---
+++ basics/src/bin/revisit.rs
@@ -0,0 +1,28 @@
+#![allow(dead_code)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+
+
+enum TypePointer{
+    Fast,
+    Slow,
+}
+
+struct Node{
+    val: i32,
+    next : *const Node,
+}
+
+fn main() {
+
+    println!("Hello World!");
+
+    const ARR_LEN:usize = 5;
+    let a:[i32;ARR_LEN] = [1,2,3,4,5];
+
+    // for i in 1..ARR_LEN{
+    //     let node[0] 
+    // }
+
+}
